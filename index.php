b83467f66730a4d1b55e730fcf598ed6a977c665
chloebernard020 <104193597+chloebernard020@users.noreply.github.com>
2022-04-22 11:45:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -17,12 +17,27 @@ if($BDD) {
         <?php require_once "header.php"; ?>
 
         <h2 class="text-center">SuperSite</h2>
+        <table>
         <?php foreach ($curseur as $histoire) { ?>
             <article>
-                <h3><a class="histTitle" href="hist.php?id=<?= $histoire['id_hist'] ?>"><?= $histoire['titre'] ?></a></h3>
-                <p class="histContent"><?= $histoire['description'] ?></p>
+                <tr>
+                    <td>
+                        <img class="img-responsive" src="img/<?= $histoire['image'] ?>" title="<?= $histoire['titre'] ?>" />
+                    </td>
+                    <td>
+                        <h3><a class="histTitle" href="hist.php?id=<?= $histoire['id_hist'] ?>"><?= $histoire['titre'] ?></a></h3>
+                    </td>
+                </tr>
+                <tr>
+                    
+                    <td>
+                        <p class="histContent"><?= $histoire['description'] ?></p>
+                    </td>
+                </tr>
+  
             </article>
         <?php } ?>
+        </table>
 
         <?php require_once "footer.php"; ?>
     </div>
