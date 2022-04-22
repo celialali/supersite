da7f2c0890a95a068faef52b267997d0de34621b
chloebernard020 <104193597+chloebernard020@users.noreply.github.com>
2022-04-22 10:49:33 +0200

    Supersite BDD

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!doctype html>
+<?php session_start(); ?>
 <html>
     <?php require "head.php";?>
     
@@ -7,18 +8,12 @@
         <?php require_once "header.php"; ?>
 
         <h2 class="text-center">SuperSite</h2>
-        <div class="container bg-grey">
-            <div class="row">
-                <div class="col">Histoire 1</div>
-                <div class="col">Histoire 2</div>
-            </div>
-            <div class = "row">
-                <div class="col">Histoire 3</div>
-            </div>
-
-            </div>
-
-        </div>
+        <?php foreach ($histoires as $histoire) { ?>
+            <article>
+                <h3><a class="movieTitle" href="movie.php?id=<?= $movie['mov_id'] ?>"><?= $movie['mov_title'] ?></a></h3>
+                <p class="movieContent"><?= $movie['mov_description_short'] ?></p>
+            </article>
+        <?php } ?>
 
         <?php require_once "footer.php"; ?>
     </div>
