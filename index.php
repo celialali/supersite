15758a0334699faee8f9e1c8d031810faa772a2b
chloebernard020 <104193597+chloebernard020@users.noreply.github.com>
2022-04-22 11:02:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <!doctype html>
-<?php session_start(); ?>
+<?php session_start(); 
+
+$movies = getDb()->query('select * from movie order by mov_id desc');?>
 <html>
     <?php require "head.php";?>
     
