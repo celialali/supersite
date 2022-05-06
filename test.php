a1d00b23053b22d7027e9b79ede1681e4d4b60aa
chloebernard020 <104193597+chloebernard020@users.noreply.github.com>
2022-05-06 10:52:29 +0200

    GERER HISTOIRES

---
+++ test.php
@@ -0,0 +1,41 @@
+<?php
+require('connect.php');
+require('head.php');
+if(isset($_POST['afficher'])){
+
+                            if($BDD){
+                                $id_hist=$_POST["afficher"];
+                                if($BDD){
+                                    $req_hist = "SELECT * FROM histoire WHERE id_hist=:unIDhist";
+                                    $rep_hist = $BDD->prepare($req_hist);
+                                    $rep_hist->execute(array("unIDhist"=>$id_hist));
+                                    $histoire = $rep_hist->fetch();
+                                    
+                                  
+                                    $req2 = "UPDATE histoire SET affichee=1 WHERE id_hist=:id";
+                                $hist = $BDD->prepare($req2);
+                                $hist -> execute(array(
+                                    'id'=>$id_hist));
+                               
+                                }
+                            }
+                        }
+                            if(isset($_POST['masquer'])){
+                                $id_hist=$_POST["masquer"];
+                                
+                                if($BDD){
+                                    $req_hist = "SELECT * FROM histoire WHERE id_hist=:unIDhist";
+                                    $rep_hist = $BDD->prepare($req_hist);
+                                    $rep_hist->execute(array("unIDhist"=>$id_hist));
+                                    $histoire = $rep_hist->fetch();
+                                    
+                                  
+                                    $req2 = "UPDATE histoire SET affichee=0 WHERE id_hist=:id";
+                                $hist = $BDD->prepare($req2);
+                                $hist -> execute(array(
+                                    'id'=>$id_hist));
+                               
+                                }
+                            }
+                            header("Location: gerer_hist.php");
+                            ?>
