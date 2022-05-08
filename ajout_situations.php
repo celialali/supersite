3ff2a3442a89eb7914a84cd74ca075fa0dc021ee
celialali <celia.rdc31@gmail.com>
2022-05-08 23:17:46 +0200

    Update ajout_situations.php
    ajout des cases situation finale et initiale mais pas fini de coder les requetes de modification

--- ajout_situations.php
+++ ajout_situations.php
@@ -15,9 +15,21 @@
                   
                     <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
                         <p class="text-center">Situation</p>
-                        <textarea name="situation" class="form-control" placeholder="Ecrivez le paragraphe correspondant à la situation" rows="3"></textarea><br/><br/>
+                        <textarea required name="situation" class="form-control" placeholder="Ecrivez le paragraphe correspondant à la situation" rows="3"></textarea><br/><br/>
                     </div>
+                    <div class="col-sm-8">
+                        <p>Cochez l'une de ces cases si la situation correspond à la situation initiale ou la situation finale (victoire): </p>
+                    </div>
+
+                    <div class="form-check col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+                        <input class="form-check-input" type="checkbox" value="1" id="sit_initiale" name="sit_initiale">
+                        <label class="form-check-label" for="sit_initiale"> Situation initiale</label> &emsp;
+                        <input class="form-check-input" type="checkbox" value="1" id="sit_finale" name="sit_finale">
+                        <label class="form-check-label" for="sit_finale"> Situation finale</label>
+                    </div>
+                    <br>
                     <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4 text-center">
+                        <br>
                         <button type="submit" class="btn btn-default btn-secondary"><span class="glyphicon"></span> Enregistrer</button>
                     </div>
                 </div>
@@ -30,12 +42,22 @@
             if (isset($_GET['id_hist'])){
                 $id_hist = $_GET['id_hist'];
             }
-                        if($BDD){
-                            $req = "INSERT INTO situation (paragraphe,id_hist) VALUES (:par,:id)";
-                            $prepare=$BDD ->prepare($req);
-                            $prepare -> execute(array("par"=>$_POST['situation'],"id"=>$id_hist));
-                        }
-                    }
+            if($BDD){
+                $req = "INSERT INTO situation (paragraphe,id_hist) VALUES (:par,:id)";
+                $prepare=$BDD ->prepare($req);
+                $prepare -> execute(array("par"=>$_POST['situation'],"id"=>$id_hist));
+                            
+                // if (isset($_POST['sit_initiale'])){
+                //     $req_sit_init = $BDD->prepare("UPDATE histoire SET id_sit_initiale=:sit_init WHERE id_hist=:idhist");
+                //     $req_sit_init->execute(array(
+                //         "sit_init"=>,  //faire une requete pour recuperer l'id de la situation qui vient d'etre creee
+                //         "idhist"=>$id_hist
+                //     ));
+                // }
+
+                //faire pareil pour la situation finale
+            }
+        }
                     ?>
         
         </div>
